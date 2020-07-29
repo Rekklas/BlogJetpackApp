d7406913bb3bb7c7f93c0b48f1701a3f8ed77707
Eduard Kovalchuk <eduard.kovalchuk@pluto.tv>
2020-07-29 21:10:49 +0300

    Setup navigation for authentication

--- app/src/main/java/com/rekklesdroid/blogjetpackapp/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/rekklesdroid/blogjetpackapp/ui/auth/AuthActivity.kt
@@ -1,7 +1,8 @@
-package com.rekklesdroid.blogjetpackapp
+package com.rekklesdroid.blogjetpackapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rekklesdroid.blogjetpackapp.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
